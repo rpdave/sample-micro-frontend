a9d5ca2085f5e74af22f1a66bc4d335920486c5f
Ronak Dave <ronak.dave@slalom.com>
2022-09-21 12:10:35 +1000

    +charting mfe complete

--- charting-app/src/App.js
+++ charting-app/src/App.js
@@ -1,15 +1,44 @@
-import LocalButton from './Button';
 import React from 'react';
-import { Grid, Row, Col, Paper } from '@mui/material';
+import { Grid, Paper, Typography } from '@mui/material';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip, Legend
+} from 'chart.js';
+import {
+  Doughnut
+} from 'react-chartjs-2';
 
-const App = () => (
-  <Grid container>
-    <Grid item>
-      <Paper>
-        Charting Container
-      </Paper>
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const App = () => {
+  const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+  return (
+    <Grid container>
+      <Grid item>
+        <Paper sx={{p:3, textAlign:"center"}} elevation={4}>
+          <Typography>Mixed Usage</Typography>
+          <Doughnut data={data}  />
+        </Paper>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  )
+};
 
 export default App;
