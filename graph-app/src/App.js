f3d632e3beab9d2434f4684f3d8780c21e8a0215
Ronak Dave <ronak.dave@slalom.com>
2022-09-21 12:21:46 +1000

    +graphing application

---
+++ graph-app/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Grid, Paper, Typography } from '@mui/material';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const App = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  return (
+    <Grid container>
+      <Grid item>
+        <Paper sx={{p:3, textAlign:"center"}} elevation={4}>
+          <Typography>Yearly Usage</Typography>
+          <Line options={options} data={data} />
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+};
+
+export default App;
