16b213c6480d83d8b0c4a45cec9e0bedf4b77e4a
Ronak Dave <ronak.dave@slalom.com>
2022-09-21 11:51:15 +1000

    +sample base app

---
+++ app2/src/App.js
@@ -0,0 +1,15 @@
+import LocalButton from './Button';
+import React from 'react';
+import { Grid, Row, Col, Paper } from '@mui/material';
+
+const App = () => (
+  <Grid container>
+    <Grid item>
+      <Paper>
+        Charting Container
+      </Paper>
+    </Grid>
+  </Grid>
+);
+
+export default App;
---
+++ app2/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+const dependencies = require('./package.json').dependencies
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 4002,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'charting',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Application': "./src/App"
+      },
+      shared: { 
+        react: { singleton: true, requiredVersion: dependencies['react'], },
+        'react-dom': { singleton: true, requiredVersion: dependencies['react-dom'], } 
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
