f661f362e70f00b12e791e2eda43b4742d234778
Ronak Dave <ronak.dave@slalom.com>
2022-09-23 15:32:11 +1000

    +dynamic data

--- graph-app/src/App.js
+++ graph-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Paper, Typography } from "@mui/material";
 import {
   Chart as ChartJS,
@@ -24,14 +24,6 @@ ChartJS.register(
 );
 
 const App = () => {
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-    },
-  };
   const labels = [
     "January",
     "February",
@@ -41,22 +33,49 @@ const App = () => {
     "June",
     "July",
   ];
+
+  const [graphData, setGraphData] = useState([]);
+  const [graph2Data, setGraph2Data] = useState([]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Generate some fake data
+      const fakeData = labels.map(() =>
+        faker.datatype.number({ min: 0, max: 1000 })
+      );
+      const fake2Data = labels.map(() =>
+        faker.datatype.number({ min: -100, max: 1000 })
+      );
+
+      setGraphData(fakeData);
+      setGraph2Data(fake2Data);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [graphData]);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+    },
+  };
+
   const data = {
     labels,
     datasets: [
       {
         label: "Dataset 1",
-        data: labels.map(() =>
-          faker.datatype.number({ min: -1000, max: 1000 })
-        ),
+        data: graphData,
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
       {
         label: "Dataset 2",
-        data: labels.map(() =>
-          faker.datatype.number({ min: -1000, max: 1000 })
-        ),
+        data: graph2Data,
         borderColor: "rgb(53, 162, 235)",
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
