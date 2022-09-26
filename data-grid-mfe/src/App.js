d8bbf4d88f907efa90d0e9082f71676f4ceda563
Ronak Dave <ronak.dave@slalom.com>
2022-09-26 10:41:28 +1000

    +dynamic data grid

--- data-grid-mfe/src/App.js
+++ data-grid-mfe/src/App.js
@@ -1,28 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import { faker } from "@faker-js/faker";
 
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
-  },
   {
     field: "fullName",
     headerName: "Full name",
@@ -32,29 +14,47 @@ const columns = [
     valueGetter: (params) =>
       `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
+  {
+    field: "message",
+    headerName: "Message",
+    width: 350,
+  },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+const rows = [];
+
+let index = 1;
 
 const App = () => {
+  const [dataRows, setDataRows] = useState(rows);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Generate new row of data
+      const newRow = {
+        id: index,
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        message: faker.hacker.phrase(),
+      };
+      index++;
+      const newRows = [...dataRows, newRow];
+      setDataRows(newRows);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [dataRows]);
+
   return (
     <Grid container mt={2}>
       <Grid item sx={{ height: 400, width: "100%" }}>
         <DataGrid
-          rows={rows}
+          rows={dataRows}
           columns={columns}
           pageSize={5}
-          rowsPerPageOptions={[5]}
+          rowsPerPageOptions={[5, 10, 15, 20]}
           checkboxSelection
           disableSelectionOnClick
           experimentalFeatures={{ newEditingApi: true }}
