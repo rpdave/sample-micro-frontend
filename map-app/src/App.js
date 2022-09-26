817d8bb45f74c4889b62ec25a4f8178f49ab427e
Ronak Dave <ronak.dave@slalom.com>
2022-09-26 10:25:07 +1000

    +dynamic map app

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Paper } from "@mui/material";
 import { Chart } from "react-google-charts";
+import { faker } from "@faker-js/faker";
 
-export const data = [
-  ["Country", "Popularity"],
-  ["Germany", 200],
-  ["United States", 300],
-  ["Brazil", 400],
-  ["Canada", 500],
-  ["France", 600],
-  ["RU", 700],
-];
+const genFakeData = () => {
+  return [
+    ["Country", "Population"],
+    ["Germany", faker.datatype.number({ min: 10, max: 10000 })],
+    ["United States", faker.datatype.number({ min: 10, max: 10000 })],
+    ["Brazil", faker.datatype.number({ min: 10, max: 10000 })],
+    ["Canada", faker.datatype.number({ min: 10, max: 10000 })],
+    ["France", faker.datatype.number({ min: 10, max: 10000 })],
+    ["RU", faker.datatype.number({ min: 10, max: 10000 })],
+  ];
+};
 
 const App = () => {
+  const data = [
+    ["Country", "Population"],
+    ["Germany", 200],
+    ["United States", 300],
+    ["Brazil", 400],
+    ["Canada", 500],
+    ["France", 600],
+    ["RU", 700],
+  ];
+
+  const [mapData, setMapData] = useState(data);
+  const [mapData2, setMapData2] = useState(data);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Generate some fake data
+      const fakeData = genFakeData();
+
+      const fakeData2 = genFakeDAta();
+
+      setMapData(fakeData);
+      setMapData2(fakeData2);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [mapData]);
+
   return (
     <Grid container pt={2} spacing={2}>
       <Grid item xs={12} md={6}>
@@ -33,7 +65,7 @@ const App = () => {
             chartType="GeoChart"
             width="100%"
             height="400px"
-            data={data}
+            data={mapData}
           />
         </Paper>
       </Grid>
@@ -55,7 +87,7 @@ const App = () => {
             chartType="GeoChart"
             width="100%"
             height="400px"
-            data={data}
+            data={mapData2}
           />
         </Paper>
       </Grid>
