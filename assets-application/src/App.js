77fe08bbb0d9961c541f28bdb0c5e1ed012a490a
Ronak Dave <ronak.dave@slalom.com>
2022-09-27 16:54:35 +1000

    +assets application

---
+++ assets-application/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  TextField,
+  InputAdornment,
+  Popper,
+  Paper,
+  Typography,
+} from "@mui/material";
+
+const App = () => {
+  return <Grid item></Grid>;
+};
+
+export default App;
