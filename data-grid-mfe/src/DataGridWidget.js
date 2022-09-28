8c42ab51ca613a0db1f4b0cd3060c6c147cabf24
Ronak Dave <ronak.dave@slalom.com>
2022-09-28 14:46:11 +1000

    -hook import

--- data-grid-mfe/src/DataGridWidget.js
+++ data-grid-mfe/src/DataGridWidget.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { DataGrid } from "@mui/x-data-grid";
 import { faker } from "@faker-js/faker";
