15c3f3bdacf3714da7045b6ed34f8e1a9a2c4f33
Ronak Dave <ronak.dave@slalom.com>
2022-10-06 11:31:10 +1100

    +use old router api

--- assets-application/src/App.js
+++ assets-application/src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import CreateAsset from "./CreateAsset";
 import Home from "./Home";
 
+const prefix = "assets";
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create" element={<CreateAsset />} />
+      <Route path={`${prefix}/`} element={<Home />} />
+      <Route path={`${prefix}/create`} element={<CreateAsset />} />
     </Routes>
   );
 };
